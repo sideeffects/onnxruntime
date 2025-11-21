e340a3cb11150b475b39b95363c67952e3a6464a
Robert Vinluan <rvinluan@sidefx.com>
2025-11-20 23:05:55 -0500

    g++14 fix for cudnn_frontend dependency.
    Patched cudnn_utils.h to add a missing header include.
    Note that this change has already been made upstream in the
    cudnn_frontend repo.

--- cmake/external/cudnn_frontend.cmake
+++ cmake/external/cudnn_frontend.cmake
@@ -1,8 +1,17 @@
 include(FetchContent)
+
+
+if (Patch_FOUND)
+    set(CUDNN_FRONTEND_PATCH_COMMAND ${Patch_EXECUTABLE} -p1 < ${PROJECT_SOURCE_DIR}/patches/cudnn_frontend/cudnn_frontend.patch)
+else ()
+    set(CUDNN_FRONTEND_PATCH_COMMAND "")
+endif ()
+
 FetchContent_Declare(
   cudnn_frontend
   URL ${DEP_URL_cudnn_frontend}
   URL_HASH SHA1=${DEP_SHA1_cudnn_frontend}
+  PATCH_COMMAND ${CUDNN_FRONTEND_PATCH_COMMAND}
 )
 
 set(CUDNN_FRONTEND_BUILD_SAMPLES OFF)
